588132021cc0cf0b4459800295a77e3864cbb9b1
lewiih001 <lewismunyambu@gmail.com>
2022-11-26 15:29:55 +0300

    work in progress

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,24 @@
+class PlantsController < ApplicationController ::Api
+    def index
+        render json: Plant.all, status: :ok
+    end
+
+    def show
+        plant=Plant.find(params[:id])
+        if plant
+            render json: plant, status: :ok
+        else
+            render json: {error: "No plant with id #{params[:id]} found"}, status: :not_found
+        end
+    end
+
+    def create
+        plant=Plant.create(plant_params)
+        render json: plant, status: :created
+    end
+
+    private
+    def plant_params
+        params.permit :name,:image,:price
+    end
+end
